77781ffeb0247cac925608f62fa6204a4015564a
Mprystal <mjprystal@gmail.com>
2020-06-15 11:56:30 -0400

    hotfix/js file

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let editButton = document.querySelector(".profile__edit-button");
-let closedButton = document.querySelector(".popup__button");
-let popup = document.querySelector(".popup");
-let form = document.querySelector(".popup__container");
+const editButton = document.querySelector(".profile__edit-button");
+const closedButton = document.querySelector(".popup__button");
+const popup = document.querySelector(".popup");
+const form = document.querySelector(".popup__container");
+const nameInput = form.querySelector(".popup__user-input_type_name");
+const aboutInput = form.querySelector(".popup__user-input_type_about");
 
-let togglePopup = () => {
+const userName = document.querySelector(".profile__name");
+const userAbout = document.querySelector(".profile__occupation");
+
+const togglePopup = () => {
   popup.classList.toggle("popup_opened");
 };
 
@@ -14,17 +19,8 @@ closedButton.addEventListener("click", togglePopup);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let nameInput = form.querySelector(".popup__user-input_type_name");
-  let aboutInput = form.querySelector(".popup__user-input_type_about");
-
-  let userName = document.querySelector(".profile__name");
-  let userAbout = document.querySelector(".profile__occupation");
-
   userName.textContent = nameInput.value;
   userAbout.textContent = aboutInput.value;
 
   togglePopup();
-
-  nameInput.value = userName.textContent;
-  aboutInput.value = userAbout.textContent;
 });
